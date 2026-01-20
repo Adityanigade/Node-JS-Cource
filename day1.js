2d644217f8352022c964ab89869a4e9f45715b26
Adityanigade <adityanigade142@gmail.com>
2026-01-20 20:03:25 +0530

    node
    node

---
+++ day1.js
@@ -0,0 +1,21 @@
+console.log(20+20);
+
+console.log("Aditya Nigade");
+
+
+function fruit(item) {
+    console.log('Name of fruit :-' + item);
+    
+}
+fruit('Apple');
+
+
+var a = 50;
+var b = 50;
+console.log(a+b);
+
+
+//for creating txt file  filr name is test1.txt
+
+var fs = require('fs');
+fs.writeFileSync("text1.txt" , "My name is Aditya ")
